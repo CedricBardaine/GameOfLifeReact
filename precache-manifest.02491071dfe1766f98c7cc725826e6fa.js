c87f72f4f00fa1482ec2efa3722a33a318b5f282
Cedric B <cedricbardaine@gmail.com>
2019-11-14 20:53:35 +0100

    Updates

---
+++ precache-manifest.02491071dfe1766f98c7cc725826e6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd845968188d7e994e43b49a36bbe6a5",
+    "url": "/CedricBardaine/gameoflifereact/index.html"
+  },
+  {
+    "revision": "14754a9eec9015830c6c",
+    "url": "/CedricBardaine/gameoflifereact/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "3b13965d1967ce2018cd",
+    "url": "/CedricBardaine/gameoflifereact/static/css/main.0b796f25.chunk.css"
+  },
+  {
+    "revision": "14754a9eec9015830c6c",
+    "url": "/CedricBardaine/gameoflifereact/static/js/2.214f338b.chunk.js"
+  },
+  {
+    "revision": "3b13965d1967ce2018cd",
+    "url": "/CedricBardaine/gameoflifereact/static/js/main.059b10e0.chunk.js"
+  },
+  {
+    "revision": "f0d39e9516f65f87849a",
+    "url": "/CedricBardaine/gameoflifereact/static/js/runtime-main.de91980c.js"
+  }
+]);
